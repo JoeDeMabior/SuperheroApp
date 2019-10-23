e1333f4f7bf908b2561ffe34779630c680748ce4
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-10-23 14:01:41 +0300

    First commit

---
+++ app/src/main/java/dev/joe/superhero/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.joe.superhero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
